8dfd03a7db4f1871febb71d68d8e26374d077836
filssavi <filippo.savi@nottingham.edu.cn>
2021-02-13 14:52:43 +0100

    Added minor numbers for axi master 0 and 1 access

--- src/ucube_lkm.c
+++ src/ucube_lkm.c
@@ -15,7 +15,7 @@
 #include <linux/types.h>
 #include <linux/poll.h>
 
-#define MAX_DEVICES	1
+#define N_MINOR_NUMBERS	3
 
 #define KERNEL_BUFFER_LENGTH 6144
 #define KERNEL_BUFFER_SIZE KERNEL_BUFFER_LENGTH*4
@@ -30,21 +30,23 @@ static int ucube_lkm_release(struct inode *, struct file *);
 static ssize_t ucube_lkm_read(struct file *, char *, size_t, loff_t *);
 static ssize_t ucube_lkm_write(struct file *, const char *, size_t, loff_t *);
 static long ucube_lkm_ioctl(struct file *filp, unsigned int cmd, unsigned long arg);
-__poll_t ucube_lkm_poll(struct file *, struct poll_table_struct *);
+static __poll_t ucube_lkm_poll(struct file *, struct poll_table_struct *);
 int ucube_lkm_probe(struct platform_device *dev);
 int ucube_lkm_remove(struct platform_device *dev);
 
 
-dev_t device_number;
-struct class *uCube_class;
-struct uCube_device_data *dev_data;
+static dev_t device_number;
+static struct class *uCube_class;
+static struct scope_device_data *dev_data;
 
-int irq_line;
+static int irq_line;
 
 /* STRUCTURE FOR THE DEVICE SPECIFIC DATA*/
-struct uCube_device_data {
-    struct device dev;
-    struct cdev cdev;
+struct scope_device_data {
+    struct device devs[3];
+
+    struct cdev cdevs[3];
+
     u32 *read_data_buffer;
     u32 *dma_buffer;
     dma_addr_t physaddr;
@@ -70,17 +72,6 @@ static struct platform_driver ucube_lkm_platform_driver = {
 };
 
 
-/* This structure points to all of the device functions */
-static struct file_operations file_ops = {
-    .read = ucube_lkm_read,
-    .write = ucube_lkm_write,
-    .open = ucube_lkm_open,
-    .unlocked_ioctl = ucube_lkm_ioctl,
-    .poll = ucube_lkm_poll,
-    .release = ucube_lkm_release
-};
-
-
 static irqreturn_t ucube_lkm_irq(int irq, void *dev_id)
 {
     memcpy(dev_data->read_data_buffer, dev_data->dma_buffer, KERNEL_BUFFER_SIZE);
@@ -89,8 +80,10 @@ static irqreturn_t ucube_lkm_irq(int irq, void *dev_id)
 }
 
 
-__poll_t ucube_lkm_poll(struct file *flip , struct poll_table_struct * poll_struct){
-    return (POLLIN | POLLRDNORM);
+static __poll_t ucube_lkm_poll(struct file *flip , struct poll_table_struct * poll_struct){
+    __poll_t mask = 0;
+    mask |= POLLIN | POLLRDNORM;
+    return mask;
 }
 
 static long ucube_lkm_ioctl(struct file *filp, unsigned int cmd, unsigned long arg) {
@@ -139,106 +132,135 @@ static ssize_t ucube_lkm_read(struct file *flip, char *buffer, size_t count, lof
 
 
 static ssize_t ucube_lkm_write(struct file *flip, const char *buffer, size_t len, loff_t *offset) {
-    pr_info("%s: In write\n", __func__);
-    return 0;
+    int minor = MINOR(flip->f_inode->i_rdev);
+    pr_info("%s: In write with minor number %d\n", __func__, minor);
+    
+    return len;
 }
 
 
 static void free_device_data(struct device *d)
 {
     
-	struct uCube_device_data *dev_data;
+	struct scope_device_data *dev_data;
 
     pr_info("%s: In free_device_data\n", __func__);
-	dev_data = container_of(d, struct uCube_device_data, dev);
+
+    for(int i = 0; i< N_MINOR_NUMBERS; i++){
+       dev_data = container_of(d, struct scope_device_data, devs[i]);
+    }
+
 	kfree(dev_data);
 }
 
 
-static int __init ucube_lkm_init(void) {
-    int rc;
-    int major;
-    dev_t uCube_device;
-    
 
+
+/* This structure points to all of the device functions */
+static struct file_operations file_ops = {
+    .read = ucube_lkm_read,
+    .write = ucube_lkm_write,
+    .open = ucube_lkm_open,
+    .unlocked_ioctl = ucube_lkm_ioctl,
+    .poll = ucube_lkm_poll,
+    .release = ucube_lkm_release
+};
+
+
+
+
+static int __init ucube_lkm_init(void) {
+    int dev_rc, platform_rc, irq_rc;
+    int major;
+    int cdev_rcs[3];
+    dev_t devices[3];
+    const char* const device_names[] = { "uscope_data", "uscope_BUS_0", "uscope_BUS_1"}; 
+    
     /* DYNAMICALLY ALLOCATE DEVICE NUMBERS, CLASSES, ETC.*/
     pr_info("%s: In init\n", __func__);\
 
-    rc = alloc_chrdev_region(&device_number, 0, MAX_DEVICES,"uCube DMA");
+    dev_rc = alloc_chrdev_region(&device_number, 0, N_MINOR_NUMBERS,"uCube DMA");
     
-    if (rc) {
-        pr_err("%s: Failed to obtain major/minors\nError:%d\n", __func__, rc);
-        return rc;
+    if (dev_rc) {
+        pr_err("%s: Failed to obtain major/minors\nError:%d\n", __func__, dev_rc);
+        return dev_rc;
     }
 
     major = MAJOR(device_number);
     uCube_class = class_create(THIS_MODULE, "uCube_scope");
     
-    uCube_device = MKDEV(major, 0);
-   
 
-    rc = platform_driver_probe(&ucube_lkm_platform_driver, ucube_lkm_probe);
+    for(int i = 0; i< N_MINOR_NUMBERS; i++){
+        devices[i] = MKDEV(major, i);
+    }
+
+    platform_rc = platform_driver_probe(&ucube_lkm_platform_driver, ucube_lkm_probe);
+    if (platform_rc) {
+        pr_err("%s: Failed to initialize platform driver\nError:%d\n", __func__, platform_rc);
+        return platform_rc;
+    }
 
     dev_data = kzalloc(sizeof(*dev_data), GFP_KERNEL);
     dev_data->new_data_available = 0;
-    dev_data->dev.devt = uCube_device;
-    dev_data->dev.class = uCube_class;
-    dev_data->dev.release = free_device_data;
-    dev_set_name(&dev_data->dev, "uCube device");
-    device_initialize(&dev_data->dev);
 
-    cdev_init(&dev_data->cdev, &file_ops);
-    rc = cdev_add(&dev_data->cdev, uCube_device, 1);
-    if (rc) {
-        pr_info("%s: Failed in adding cdev to subsystem "
-                "retval:%d\n", __func__, rc);
-    }
-    else {
-        device_create(uCube_class, NULL, uCube_device, NULL, "uCube_dev_%d", 0);
+    for(int i = 0; i< N_MINOR_NUMBERS; i++){
+        dev_data->devs[i].devt =  devices[i];
+        dev_data->devs[i].class = uCube_class;
+        dev_data->devs[i].release = free_device_data;
+        dev_set_name(&dev_data->devs[i], device_names[i]);
+        device_initialize(&dev_data->devs[i]);
+        cdev_init(&dev_data->cdevs[i], &file_ops);
+        cdev_rcs[i] = cdev_add(&dev_data->cdevs[i], devices[i], 1);
+        if (cdev_rcs[i]) {
+        pr_info("%s: Failed in adding cdev[%d] to subsystem "
+                "retval:%d\n", __func__, i, cdev_rcs[i]);
+        }
+        else {
+            device_create(uCube_class, NULL, devices[i], NULL, device_names[i]);
+        }
+        pr_info("%s: finished setup for endpoint: %s\n", __func__, device_names[i]);
     }
     
     /*SETUP AND ALLOCATE DMA BUFFER*/
-    dma_set_coherent_mask(&dev_data->dev, DMA_BIT_MASK(32));
-    dev_data->dma_buffer = dma_alloc_coherent(&dev_data->dev, KERNEL_BUFFER_LENGTH*sizeof(int), &(dev_data->physaddr), GFP_KERNEL ||GFP_ATOMIC);
+    dma_set_coherent_mask(&dev_data->devs[0], DMA_BIT_MASK(32));
+    dev_data->dma_buffer = dma_alloc_coherent(&dev_data->devs[0], KERNEL_BUFFER_LENGTH*sizeof(int), &(dev_data->physaddr), GFP_KERNEL ||GFP_ATOMIC);
     pr_warn("%s: Allocated dma buffer at: %u\n", __func__, dev_data->physaddr);
     /*SETUP AND ALLOCATE DATA BUFFER*/
     dev_data->read_data_buffer = vmalloc(KERNEL_BUFFER_SIZE);
     
-    if (rc) {
-        pr_err("%s: Failed to initialize platform driver\nError:%d\n", __func__, rc);
-        return rc;
-    }
+
     /* SETUP INTERRUPT HANDLER*/      
     pr_warn("%s: setup interrupts\n", __func__);
-    rc = request_irq(irq_line, ucube_lkm_irq, 0, "ucube_lkm", NULL);
+    irq_rc = request_irq(irq_line, ucube_lkm_irq, 0, "ucube_lkm", NULL);
     //pr_warn("%s: unassigned irqs: %lu\n", __func__, probe_irq_on());
     
-    return rc;
+    return irq_rc;
 }
 
-static void __exit ucube_lkm_exit(void) {int i = 0;
-	
+static void __exit ucube_lkm_exit(void) {
+
 	int major = MAJOR(device_number);
-	dev_t my_device;
     
     pr_info("%s: In exit\n", __func__);
     free_irq(irq_line, NULL);
 
-    dma_free_coherent(&dev_data->dev,KERNEL_BUFFER_LENGTH*sizeof(int),dev_data->dma_buffer, dev_data->physaddr);
+    dma_free_coherent(&dev_data->devs[0],KERNEL_BUFFER_LENGTH*sizeof(int),dev_data->dma_buffer, dev_data->physaddr);
     vfree(dev_data->read_data_buffer);
     
     platform_driver_unregister(&ucube_lkm_platform_driver);	
     
-    my_device = MKDEV(major, i);
-    cdev_del(&dev_data->cdev);
-    device_destroy(uCube_class, my_device);
+
+    for(int i = 0; i< N_MINOR_NUMBERS; i++){
+        int device = MKDEV(major, i);
+
+        cdev_del(&dev_data->cdevs[i]);
+        device_destroy(uCube_class, device);
+    }
+
 
 	class_destroy(uCube_class);
-	unregister_chrdev_region(device_number, MAX_DEVICES);
+	unregister_chrdev_region(device_number, N_MINOR_NUMBERS);
     
-    
-    
-
 }
 
 int ucube_lkm_probe(struct platform_device *dev){
